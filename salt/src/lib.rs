2d0e9c2ab36421cc1da7808d91d6bddfd1e23c31
Caleb Yates <calebyates42@gmail.com>
2025-05-25 22:34:44 +1000

    todo: add tracing logging

--- salt/src/lib.rs
+++ salt/src/lib.rs
@@ -11,11 +11,15 @@ pub mod prelude {
 use std::process::ExitStatus;
 
 use camino::FromPathBufError;
+use cli::Command;
+use git::Git;
+use url::Url;
+use which::which;
 
 use crate::prelude::*;
 
 pub struct Salt {
-	path: Utf8PathBuf,
+	project_folder: Utf8PathBuf,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -37,24 +41,79 @@ pub enum Error {
 	#[error("Subprocess exited badly: {0:?}")]
 	SubprocessExited(ExitStatus),
 
-	#[error("{0}")]
-	Which(#[from] ::which::Error),
+	#[error(
+		"Expected `{bin_name}` binary to be in PATH environment variable or finable with which https://docs.rs/which/latest/which/fn.which.html ({err_msg}): {which}"
+	)]
+	Which {
+		bin_name: String,
+		err_msg: String,
+		which: ::which::Error,
+	},
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
 
 impl Salt {
-	fn default_path() -> Result<Utf8PathBuf> {
+	fn default_project_path() -> Result<Utf8PathBuf> {
 		let dir = dirs::data_local_dir()
 			.or(dirs::data_dir())
-			.ok_or(Error::NoStandardDirectoryFound);
-		Ok(Utf8PathBuf::try_from(dir?)?)
+			.ok_or(Error::NoStandardDirectoryFound)?;
+		let path = Utf8PathBuf::try_from(dir)?;
+		Ok(path.join("salt-asset-manager"))
 	}
 
 	pub fn new() -> Result<Salt> {
-		Ok(Salt {
-			path: Salt::default_path()?,
-		})
+		let salt = Salt {
+			project_folder: Salt::default_project_path()?,
+		};
+
+		Ok(salt)
+	}
+
+	pub fn transaction(
+		&self,
+		amount: String,
+		vault_address: String,
+		recipient_address: String,
+	) -> Result<()> {
+		self.cmd([
+			"-amount".into(),
+			amount,
+			"-vault-address".into(),
+			vault_address,
+			"-recipient-address".into(),
+			recipient_address,
+		])?
+		.run_and_wait()?;
+		Ok(())
+	}
+
+	fn cmd(&self, args: impl IntoIterator<Item = String>) -> Result<Command> {
+		let git = self.git()?;
+		git.ensure_latest_branch(
+			Url::parse("https://github.com/ActuallyHappening/salt-asset-manager").unwrap(),
+			"master",
+		)?;
+		let deno = which("deno", "required javascript runtime")?;
+		let cmd = cli::Command::pure(deno)?
+			.with_args(
+				[
+					"run",
+					"--unstable-sloppy-imports",
+					"-A",
+					"src/index.ts",
+					"--",
+					"-use-cli-only",
+				]
+				.into_iter()
+				.map(String::from),
+			)
+			.with_args(args);
+		Ok(cmd)
+	}
+
+	fn git(&self) -> Result<Git> {
+		Ok(Git::new(self.project_folder.to_owned())?)
 	}
 }
 
@@ -105,44 +164,60 @@ mod git {
 	use crate::{cli::Command, prelude::*, which::which};
 
 	pub struct Git {
-		path: Utf8PathBuf,
+		project_folder: Utf8PathBuf,
 	}
 
 	impl Git {
 		pub fn new(path: Utf8PathBuf) -> Result<Self> {
-			Ok(Self { path })
+			Ok(Self {
+				project_folder: path,
+			})
 		}
 
 		fn cmd(&self) -> Result<Command> {
-			let git = which("git").inspect_err(|_| {
-				error!(
-					"A `git` binary available on PATH is a required runtime dependency of https://lib.rs/salt-sdk"
-				)
-			})?;
-			let cmd = Command::pure(git)?.with_cwd(self.path.to_owned());
+			let git = which("git", "required runtime dependency")?;
+			let cmd = Command::pure(git)?.with_cwd(self.project_folder.to_owned());
 			Ok(cmd)
 		}
 
-		pub fn clone_and_or_pull(&self, repository_url: Url) -> Result<()> {
-			if self.path.exists() {
+		pub fn ensure_latest_branch(&self, repository_url: Url, branch: &str) -> Result<()> {
+			if self.project_folder.exists() {
 				// assume already checked out
-				self.pull()
+				self.pull()?;
 			} else {
-				self.clone(repository_url)
+				self.clone(repository_url)?;
 			}
+			self.checkout(branch)?;
+			Ok(())
 		}
 
 		fn pull(&self) -> Result<()> {
+			debug!("Running `git pull` in directory {}", &self.project_folder);
 			self.cmd()?.with_args(["pull".into()]).run_and_wait()
 		}
 
 		fn clone(&self, repository_url: Url) -> Result<()> {
+			let mut parent_folder = self.project_folder.clone();
+			if !parent_folder.pop() {
+				panic!("self.project_folder has no parent? Why is the data dir at / ?");
+			}
+			debug!(
+				"Running `git clone {}` in directory {}",
+				repository_url, &parent_folder
+			);
 			self.cmd()?
+				.with_cwd(parent_folder)
 				.with_args([
 					"clone".into(),
 					repository_url.to_string(),
-					self.path.to_string(),
+					self.project_folder.to_string(),
 				])
+				.run_and_wait()
+		}
+
+		fn checkout(&self, branch: &str) -> Result<()> {
+			self.cmd()?
+				.with_args(["checkout".into(), branch.into()])
 				.run_and_wait()
 		}
 	}
@@ -151,8 +226,12 @@ mod git {
 mod which {
 	use crate::prelude::*;
 
-	pub fn which(name: &'static str) -> Result<Utf8PathBuf> {
-		let path = ::which::which(name).inspect_err(|_| {})?;
+	pub fn which(name: &'static str, err_msg: impl Into<String>) -> Result<Utf8PathBuf> {
+		let path = ::which::which(name).map_err(|which| Error::Which {
+			bin_name: name.to_owned(),
+			err_msg: err_msg.into(),
+			which,
+		})?;
 		Ok(Utf8PathBuf::try_from(path)?)
 	}
 }
