f26719e88542cbb2a98ec4c9331c727c141d8f1b
Caleb Yates <calebyates42@gmail.com>
2025-05-25 22:15:39 +1000

    add: git cli interface

--- salt/src/lib.rs
+++ salt/src/lib.rs
@@ -32,7 +32,7 @@ pub enum Error {
 	ExecutableFileDoesntExist(Utf8PathBuf),
 
 	#[error("{0}")]
-	FailedToExecute(#[from] bossy::Error),
+	FailedToExecute(std::io::Error),
 
 	#[error("Subprocess exited badly: {0:?}")]
 	SubprocessExited(ExitStatus),
@@ -61,23 +61,36 @@ impl Salt {
 mod cli {
 	use crate::prelude::*;
 
-	#[derive(Clone)]
-	pub struct Command(bossy::Command);
+	pub struct Command(std::process::Command);
 
 	impl Command {
 		pub fn pure(cmd: Utf8PathBuf) -> Result<Command> {
 			if !cmd.exists() {
 				return Err(Error::ExecutableFileDoesntExist(cmd));
 			}
-			Ok(Command(bossy::Command::pure(cmd)))
+			let mut cmd = std::process::Command::new(cmd);
+			cmd.env_clear();
+			Ok(Command(cmd))
 		}
 
-		pub fn with_args(self, args: impl IntoIterator<Item = String>) -> Self {
-			Self(self.0.with_args(args))
+		pub fn current_dir(&mut self, cwd: Utf8PathBuf) -> &mut Self {
+			self.0.current_dir(cwd);
+			self
+		}
+
+		pub fn with_cwd(mut self, cwd: Utf8PathBuf) -> Self {
+			self.current_dir(cwd);
+			self
+		}
+
+		pub fn with_args(mut self, args: impl IntoIterator<Item = String>) -> Self {
+			self.0.args(args);
+			self
 		}
 
 		pub fn run_and_wait(mut self) -> Result<()> {
-			let status = self.0.run_and_wait()?;
+			info!("Running command {:?}", self.0);
+			let status = self.0.status().map_err(Error::FailedToExecute)?;
 			if !status.success() {
 				return Err(Error::SubprocessExited(status));
 			}
@@ -91,38 +104,40 @@ mod git {
 
 	use crate::{cli::Command, prelude::*, which::which};
 
-	#[derive(Clone)]
 	pub struct Git {
 		path: Utf8PathBuf,
-		/// default cmd with no args always NB
-		cmd: Command,
 	}
 
 	impl Git {
 		pub fn new(path: Utf8PathBuf) -> Result<Self> {
+			Ok(Self { path })
+		}
+
+		fn cmd(&self) -> Result<Command> {
 			let git = which("git").inspect_err(|_| {
 				error!(
 					"A `git` binary available on PATH is a required runtime dependency of https://lib.rs/salt-sdk"
 				)
 			})?;
-			let cmd = Command::pure(git)?;
-			Ok(Git { cmd, path })
+			let cmd = Command::pure(git)?.with_cwd(self.path.to_owned());
+			Ok(cmd)
 		}
 
-		pub fn clone_and_or_pull(&self, repository_url: Url) {
+		pub fn clone_and_or_pull(&self, repository_url: Url) -> Result<()> {
 			if self.path.exists() {
 				// assume already checked out
-				self.pull();
+				self.pull()
+			} else {
+				self.clone(repository_url)
 			}
 		}
 
 		fn pull(&self) -> Result<()> {
-			self.cmd.clone().with_args(["pull".into()]).run_and_wait()
+			self.cmd()?.with_args(["pull".into()]).run_and_wait()
 		}
 
 		fn clone(&self, repository_url: Url) -> Result<()> {
-			self.cmd
-				.clone()
+			self.cmd()?
 				.with_args([
 					"clone".into(),
 					repository_url.to_string(),
