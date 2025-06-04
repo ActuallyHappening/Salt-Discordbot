a818604ca96f279fe47b5a5704d2535713f5cf56
Caleb Yates <calebyates42@gmail.com>
2025-06-04 21:50:49 +1000

    add: describe internal tools

--- src/ratelimits.rs
+++ src/ratelimits.rs
@@ -4,6 +4,10 @@ use time::{Duration, OffsetDateTime};
 
 use crate::prelude::*;
 
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
+#[serde(try_from = "ser::RateLimits", into = "ser::RateLimits")]
+pub struct RateLimits(HashMap<u64, ChainLimits>);
+
 #[derive(serde::Deserialize, serde::Serialize, Clone, Default)]
 struct ChainLimits {
 	address: HashMap<Box<str>, Vec<OffsetDateTime>>,
@@ -20,8 +24,26 @@ fn chain_limits_serde() {
 	let toml = r##"
 		"##;
 	let _: RateLimits = toml::from_str(toml).expect("to deserialize");
-	
+
 	RateLimits::read().expect("to deserialize");
+}
+
+#[test]
+fn server_limits() {
+	let toml = std::fs::read_to_string(concat!(
+		env!("CARGO_MANIFEST_DIR"),
+		"/server-ratelimits.toml"
+	))
+	.unwrap();
+	let mut ratelimits: RateLimits = toml::from_str(&toml).expect("to deserialize");
+	ratelimits
+		.check(&Key {
+			address: todo!(),
+			discord_id: todo!(),
+			chain_id: todo!(),
+			chain_name: todo!(),
+		})
+		.expect("To not be ratelimited");
 }
 
 pub struct Key {
@@ -30,10 +52,6 @@ pub struct Key {
 	pub chain_id: u64,
 	pub chain_name: String,
 }
-
-#[derive(serde::Deserialize, serde::Serialize, Clone)]
-#[serde(try_from = "ser::RateLimits", into = "ser::RateLimits")]
-pub struct RateLimits(HashMap<u64, ChainLimits>);
 
 /// Doesn't use u64 as key
 mod ser {
@@ -85,6 +103,17 @@ impl RateLimits {
 			.check(&key.address, &key.discord_id, &key.chain_name)
 	}
 
+	fn describe(&mut self, discord_id: &str) -> String {
+		let now = OffsetDateTime::now_utc();
+		let mut ret = String::new();
+		for (chain_id, chain_limits) in &mut self.0 {
+			ret.push_str(&format!("Chain ID: {}\n", chain_id));
+			ret.push_str(&chain_limits.describe(&now, discord_id));
+			ret.push('\n');
+		}
+		ret
+	}
+
 	pub fn register(&mut self, key: &Key) -> Result<()> {
 		if !self.0.contains_key(&key.chain_id) {
 			self.0.insert(key.chain_id, ChainLimits::default());
@@ -102,6 +131,15 @@ impl RateLimits {
 #[derive(Debug, thiserror::Error)]
 #[error("{0}")]
 pub struct RateLimitErr(String);
+
+fn format(date: OffsetDateTime) -> String {
+	let format = time::format_description::parse(
+		"[year]-[month]-[day] [hour]:[minute]:[second] [offset_hour \
+        sign:mandatory]:[offset_minute]:[offset_second]",
+	)
+	.unwrap();
+	date.format(&format).unwrap()
+}
 
 /// Simple toml file storage
 impl RateLimits {
@@ -121,7 +159,12 @@ impl RateLimits {
 }
 
 impl ChainLimits {
-	pub fn check(&mut self, address: &str, discord_id: &str, chain_name: &str) -> Result<(), RateLimitErr> {
+	pub fn check(
+		&mut self,
+		address: &str,
+		discord_id: &str,
+		chain_name: &str,
+	) -> Result<(), RateLimitErr> {
 		let address: Box<str> = address.to_owned().into_boxed_str();
 		let discord_id = discord_id.to_owned().into_boxed_str();
 		let now = OffsetDateTime::now_utc();
@@ -136,7 +179,7 @@ impl ChainLimits {
 		}
 		let discord_valid = Self::discord_id_valid(&now, self.discord_id.get(&discord_id).unwrap());
 
-		match (address_valid, discord_valid) {
+		match (address_valid.is_ok(), discord_valid.is_ok()) {
 			(true, false) => Err(RateLimitErr(format!(
 				"You've reached your limit for fauceting for your discord account on {chain_name}. Please try again in 24 hours.",
 			))),
@@ -148,6 +191,23 @@ impl ChainLimits {
 			))),
 			(true, true) => Ok(()),
 		}
+	}
+
+	fn describe(&mut self, now: &OffsetDateTime, discord_id: &str) -> String {
+		let mut ret = String::new();
+		for (address, records) in self.address.clone() {
+			let records = records
+				.into_iter()
+				.map(|date| format(date.clone()))
+				.collect::<Vec<String>>();
+			ret.push_str(&format!(
+				"Address (ratelimited: {:?}): {:?}",
+				self.check(&address, discord_id, "").is_ok(),
+				records
+			));
+		}
+		ret.push_str("\n");
+		ret
 	}
 
 	/// Automatically saves
@@ -167,37 +227,39 @@ impl ChainLimits {
 		self.discord_id.get_mut(&discord_id).unwrap().push(now);
 	}
 
-	fn address_valid(now: &OffsetDateTime, previous: &Vec<OffsetDateTime>) -> bool {
+	/// Return Ok(_) or Err(_) with the relevant datetimes under consideration
+	fn address_valid(
+		now: &OffsetDateTime,
+		previous: &[OffsetDateTime],
+	) -> Result<Vec<OffsetDateTime>, Vec<OffsetDateTime>> {
 		// 2 per day
 		let range = Duration::DAY;
 		let max_num_in_range = 2;
 
-		let previous_num = previous
-			.iter()
-			.filter(|time| (**time - *now) < range)
-			.count();
+		let previous_num = previous.iter().filter(|time| (**time - *now) < range);
 
-		if previous_num >= max_num_in_range {
-			false
+		if previous_num.clone().count() >= max_num_in_range {
+			Err(previous_num.cloned().collect())
 		} else {
-			true
+			Ok(previous_num.cloned().collect())
 		}
 	}
 
-	fn discord_id_valid(now: &OffsetDateTime, previous: &Vec<OffsetDateTime>) -> bool {
+	/// Return Ok(_) or Err(_) with the relevant datetimes under consideration
+	fn discord_id_valid(
+		now: &OffsetDateTime,
+		previous: &Vec<OffsetDateTime>,
+	) -> Result<Vec<OffsetDateTime>, Vec<OffsetDateTime>> {
 		// 3 per day
 		let range = Duration::DAY;
 		let max_num_in_range = 3;
 
-		let previous_num = previous
-			.iter()
-			.filter(|time| (**time - *now) < range)
-			.count();
+		let previous_num = previous.iter().filter(|time| (**time - *now) < range);
 
-		if previous_num >= max_num_in_range {
-			false
+		if previous_num.clone().count() >= max_num_in_range {
+			Err(previous_num.cloned().collect())
 		} else {
-			true
+			Ok(previous_num.cloned().collect())
 		}
 	}
 }
