def00abcd740beaa66ba534e52b62f6a0b9d14c5
Caleb Yates <calebyates42@gmail.com>
2025-06-07 17:38:05 +1000

    todo: work out how to properly parse private key material

--- src/bin/tokens.rs
+++ src/bin/tokens.rs
@@ -1,15 +1,22 @@
-use alloy::{providers::ProviderBuilder, sol};
 #[allow(unused_imports)]
 use ::tracing::{debug, error, info, trace, warn};
+use alloy::{
+	primitives::{FixedBytes, Uint, address, utils::Unit},
+	providers::ProviderBuilder,
+	signers::k256::sha2::digest::typenum::UInt,
+	sol,
+};
 use color_eyre::eyre::Context as _;
 
 #[path = "../tracing.rs"]
 mod app_tracing;
 
 /// https://shannon-explorer.somnia.network/token/0x33E7fAB0a8a5da1A923180989bD617c9c2D1C493
-const PING: alloy::primitives::Address = alloy::primitives::address!("0x33E7fAB0a8a5da1A923180989bD617c9c2D1C493");
+const PING: alloy::primitives::Address =
+	alloy::primitives::address!("0x33E7fAB0a8a5da1A923180989bD617c9c2D1C493");
 /// https://shannon-explorer.somnia.network/token/0x9beaA0016c22B646Ac311Ab171270B0ECf23098F
-const PONG: alloy::primitives::Address = alloy::primitives::address!("0x9beaA0016c22B646Ac311Ab171270B0ECf23098F");
+const PONG: alloy::primitives::Address =
+	alloy::primitives::address!("0x9beaA0016c22B646Ac311Ab171270B0ECf23098F");
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
@@ -39,19 +46,40 @@ async fn main() -> color_eyre::Result<()> {
 	// #[allow(unused)]
 	// let mut rl = rustyline::DefaultEditor::new()?;
 
-	let provider = alloy::providers::ProviderBuilder::new().connect(somnia_rpc.as_str()).await?;
-	let erc20_ping = ERC20::new(PING, provider.clone());
-	let erc20_pong = ERC20::new(PONG, provider);
+	{
+		let provider = alloy::providers::ProviderBuilder::new()
+			.connect(somnia_rpc.as_str())
+			.await?;
+		let erc20_ping = ERC20::new(PING, provider.clone());
+		let erc20_pong = ERC20::new(PONG, provider);
 
-	let owner = "0xEA428233445A5Cf500B9d5c91BcA6E7B887f7D70".parse()?;
-	let balance_ping = erc20_ping.balanceOf(owner).call().await?;
-	let balance_pong = erc20_pong.balanceOf(owner).call().await?;
+		let owner = "0xEA428233445A5Cf500B9d5c91BcA6E7B887f7D70".parse()?;
+		let balance_ping = erc20_ping.balanceOf(owner).call().await?;
+		let balance_pong = erc20_pong.balanceOf(owner).call().await?;
 
-	info!("You have {} PING, {} PONG", balance_ping, balance_pong);
-	
-	let swap = "0x6AAC14f090A35EeA150705f72D90E4CDC4a49b2C";
+		info!("You have {} PING, {} PONG", balance_ping, balance_pong);
+	}
 
-	// erc20_ping.transfer(recipient, amount).calldata();
+	// let swap = "0x6AAC14f090A35EeA150705f72D90E4CDC4a49b2C";
+
+	{
+		let from = address!("0xEA428233445A5Cf500B9d5c91BcA6E7B887f7D70");
+		let to = address!("0x85BCADfB48E95168b3C4aA3221ca2526CF96c99E");
+		let amount = ("0.5", Unit::ETHER);
+		let amount = alloy::primitives::utils::ParseUnits::parse_units(amount.0, amount.1)
+			.unwrap()
+			.get_absolute();
+
+		let signer: alloy::signers::local::PrivateKeySigner = env.private_key.parse()?;
+		debug!(?signer);
+		let provider = alloy::providers::ProviderBuilder::new()
+			.wallet(signer)
+			.connect(somnia_rpc.as_str())
+			.await?;
+		let erc20_ping = ERC20::new(PING, provider.clone());
+
+		erc20_ping.transferFrom(from, to, amount).call().await?;
+	}
 
 	Ok(())
 }
