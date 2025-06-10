f511c9fd96a4b54ffb84bc3995a87e2a1e4dbde4
Caleb Yates <calebyates42@gmail.com>
2025-06-10 20:45:34 +1000

    todo: encode transaction and send to smart contract

--- intu-sdk/src/lib.rs
+++ intu-sdk/src/lib.rs
@@ -1,8 +1,34 @@
-use alloy::sol;
+/// Generated contract ABI code.
+/// Wow Rust is truly awesome!
+pub mod abi {
+	use alloy::sol;
+	sol!(
+		#[sol(rpc, abi)]
+		#[derive(Debug)]
+		IntuVault,
+		concat!(env!("CARGO_MANIFEST_DIR"), "/abi/Vault.json")
+	);
+}
 
-sol!(
-	#[sol(rpc, abi)]
-	#[derive(Debug)]
-	IntuVault,
-	concat!(env!("CARGO_MANIFEST_DIR"), "/abi/Vault.json")
-);
+/// Here for completeness not usability
+pub mod other {
+	pub mod factory {
+		use alloy::sol;
+		sol!(
+			#[sol(rpc, abi)]
+			#[derive(Debug)]
+			VaultFactory,
+			concat!(env!("CARGO_MANIFEST_DIR"), "/abi/VaultFactory.json")
+		);
+	}
+
+	pub mod fee {
+		use alloy::sol;
+		sol!(
+			#[sol(rpc, abi)]
+			#[derive(Debug)]
+			VaultFactory,
+			concat!(env!("CARGO_MANIFEST_DIR"), "/abi/Fee.json")
+		);
+	}
+}
--- intu-sdk/src/main.rs
+++ intu-sdk/src/main.rs
@@ -1,8 +1,5 @@
 use alloy::{
-	primitives::{Address, address},
-	providers::Provider,
-	signers::local::PrivateKeySigner,
-	sol,
+	eips::Encodable2718, network::{EthereumWallet, TransactionBuilder}, primitives::{address, utils::parse_ether, Address, Bytes, U256}, providers::Provider, rpc::types::TransactionRequest, signers::local::PrivateKeySigner, sol
 };
 use tracing::{debug, error, info, trace, warn};
 
@@ -16,15 +13,35 @@ const SEPOLIA_ARBITRUM_RPC: &str = "https://sepolia-rollup.arbitrum.io/rpc";
 async fn main() -> color_eyre::Result<()> {
 	app_tracing::install_tracing("info,intu_sdk=trace")?;
 
-	// let signer: PrivateKeySigner = include_str!("private_key").parse()?;
+	let signer: PrivateKeySigner = include_str!("private_key").parse()?;
+	let wallet = EthereumWallet::new(signer);
 	let provider = alloy::providers::ProviderBuilder::new()
+		.wallet(wallet.clone())
 		.connect(SEPOLIA_ARBITRUM_RPC)
 		.await?;
 
-	let intu_contract = intu_sdk::IntuVault::new(VAULT, provider);
+	let intu_contract = intu_sdk::abi::IntuVault::new(VAULT, provider);
 
-	let info = intu_contract.vaultInfos().call().await?;
-	info!("Found info for vault in Rust: {:?}", info);
+	let vault_info = intu_contract.vaultInfos().call().await?;
+	info!("Found info for vault in Rust: {:?}", vault_info);
+
+	let transactions = intu_contract.transactions(U256::from(84)).call().await?;
+	info!(?transactions);
+
+	let me = address!("0xEA428233445A5Cf500B9d5c91BcA6E7B887f7D70");
+	let amount = parse_ether("0.5");
+	let tx = TransactionRequest::default()
+		.with_to(me)
+		.with_chain_id(421614)
+		.build(&wallet)
+		.await?;
+	let tx = tx.encoded_2718();
+
+	// let test_transaction = TransactionBuilder {};
+	let pending = intu_contract
+		.proposeTransaction(transactionInfo, String::from("first time?"))
+		.send()
+		.await?;
 
 	Ok(())
 }
